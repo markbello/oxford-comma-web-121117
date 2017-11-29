7631631036300fbfec1067ad84c8cc8726b12d6e
Mark Bello <mcbello51286@gmail.com>
2017-11-29 16:02:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
       when 1
         return array[0]
       when 2
-        return array[0] + " and " + array[1]
+        return "#{array[0]} and #{array[1]}}"
+      when 3
+        return "#{array[0]}, #{array[1]}, and #{array[2]}"
     end
 end
