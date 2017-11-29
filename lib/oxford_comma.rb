85922e901fdd1f8f2886188d8bb3a26305588455
Mark Bello <mcbello51286@gmail.com>
2017-11-29 16:08:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+    case array.count
+      when 0
+        return
+      when 1
+        return array[0]
+      when 2
+        return "#{array[0]} and #{array[1]}"
+      when 3
+        return "#{array[0]}, #{array[1]}, and #{array[2]}"
+      else
+        phrase = ""
+        array.each_with_index{|item, i|
+        if i < array.count - 1
+          phrase += "#{item}, "
+        else
+          phrase += "and #{item}"
+        end
+        }
+        return phrase
+    end
 end
